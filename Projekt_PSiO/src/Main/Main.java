f16042b05ddbe3356702247c2502fb599a86c4d6
StudentBJ <stawiamklocki@onet.pl>
2024-12-13 15:27:22 +0100

    Add files via upload

---
+++ Projekt_PSiO/src/Main/Main.java
@@ -0,0 +1,81 @@
+import Adres.*;
+import Logowanie.*;
+import Osoba.*;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("  ______   __    __  __        ________  _______                                          \n" +
+                    " /      \\ |  \\  /  \\|  \\      |        \\|       \\                                         \n" +
+                    "|  $$$$$$\\| $$ /  $$| $$      | $$$$$$$$| $$$$$$$\\                                        \n" +
+                    "| $$___\\$$| $$/  $$ | $$      | $$__    | $$__/ $$                                        \n" +
+                    " \\$$    \\ | $$  $$  | $$      | $$  \\   | $$    $$                                        \n" +
+                    " _\\$$$$$$\\| $$$$$\\  | $$      | $$$$$   | $$$$$$$                                         \n" +
+                    "|  \\__| $$| $$ \\$$\\ | $$_____ | $$_____ | $$                                              \n" +
+                    " \\$$    $$| $$  \\$$\\| $$     \\| $$     \\| $$                                              \n" +
+                    "  \\$$$$$$  \\$$   \\$$ \\$$$$$$$$ \\$$$$$$$$ \\$$                                              \n" +
+                    "                                                                                          \n" +
+                    "                                                                                          \n" +
+                    "                                                                                          \n" +
+                    "  ______   __        __        ________  _______   _______    ______    ______    ______  \n" +
+                    " /      \\ |  \\      |  \\      |        \\|       \\ |       \\  /      \\  /      \\  /      \\ \n" +
+                    "|  $$$$$$\\| $$      | $$      | $$$$$$$$| $$$$$$$\\| $$$$$$$\\|  $$$$$$\\|  $$$$$$\\|  $$$$$$\\\n" +
+                    "| $$__| $$| $$      | $$      | $$__    | $$  | $$| $$__| $$| $$  | $$| $$ __\\$$| $$  | $$\n" +
+                    "| $$    $$| $$      | $$      | $$  \\   | $$  | $$| $$    $$| $$  | $$| $$|    \\| $$  | $$\n" +
+                    "| $$$$$$$$| $$      | $$      | $$$$$   | $$  | $$| $$$$$$$\\| $$  | $$| $$ \\$$$$| $$  | $$\n" +
+                    "| $$  | $$| $$_____ | $$_____ | $$_____ | $$__/ $$| $$  | $$| $$__/ $$| $$__| $$| $$__/ $$\n" +
+                    "| $$  | $$| $$     \\| $$     \\| $$     \\| $$    $$| $$  | $$ \\$$    $$ \\$$    $$ \\$$    $$\n" +
+                    " \\$$   \\$$ \\$$$$$$$$ \\$$$$$$$$ \\$$$$$$$$ \\$$$$$$$  \\$$   \\$$  \\$$$$$$   \\$$$$$$   \\$$$$$$ \n" +
+                    "                                                                                          \n" +
+                    "                                                                                          \n" +
+                    "                                                                                          ");
+
+            System.out.println("\nMenu logowania do sklepu");
+            System.out.println("1. Zaloguj sie (klient).");
+            System.out.println("2. Zaloguj sie (sprzedawca).");
+            System.out.println("3. Zaloguj sie (pracownik).");
+            System.out.println("4. Przeglądaj produkty.");
+            System.out.println("5. Wyszukaj dany produkt.");
+            System.out.println("6. Wyszukaj danego sprzedawcę.");
+            System.out.println("7. Wyswietl wszystkich uzytkownikow sklepu.");
+            System.out.println("8. Wyjscie ze sklepu\n");
+
+            System.out.print("Wybierz opcję: ");
+            int wybor = input.nextInt();
+
+            switch (wybor) {
+                case 1:
+                    dodajPrzykladoweTransakcje();
+                    break;
+                case 2:
+                    wyswietlTransakcje();
+                    break;
+                case 3:
+                    dodajTransakcje(scanner);
+                    break;
+                case 4:
+                    policzTransakcjeSeniora();
+                    break;
+                case 5:
+                    znajdzNajdrozszegoStudenta();
+                    break;
+                case 6:
+                    zapiszTransakcjeStudentow();
+                    break;
+                case 0:
+                    System.out.println("Koniec programu.");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Nieprawidłowy wybór!");
+
+
+
+            }
+    }
+}
+
