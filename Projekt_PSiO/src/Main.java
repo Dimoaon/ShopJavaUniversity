83bc70b57907b647d21378bf331733fe30036f5d
StudentBJ <stawiamklocki@onet.pl>
2024-12-13 15:12:42 +0100

    Main

--- Projekt_PSiO/src/Main.java
+++ Projekt_PSiO/src/Main.java
@@ -8,7 +8,28 @@ public class Sklep
 {
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
 
-    }
+        while (true) {
+            System.out.println("  _____________  __.____     _____________________ \n" +
+                    " /   _____/    |/ _|    |    \\_   _____/\\______   \\\n" +
+                    " \\_____  \\|      < |    |     |    __)_  |     ___/\n" +
+                    " /        \\    |  \\|    |___  |        \\ |    |    \n" +
+                    "/_______  /____|__ \\_______ \\/_______  / |____|    \n" +
+                    "        \\/        \\/       \\/        \\/            ");
+
+            System.out.println("\nMenu logowania do sklepu");
+            System.out.println("1. Dodaj transakcje");
+            System.out.println("2. Wyświetl transakcje");
+            System.out.println("3. Dodaj pojedynczą transakcję");
+            System.out.println("4. Policz transakcje z Kartą Seniora");
+            System.out.println("5. Znajdź klienta ze Studencką Kartą z najwyższą transakcją");
+            System.out.println("6. Zapisz transakcje studentów do pliku");
+            System.out.println("0. Wyjście");
+
+            System.out.print("Wybierz opcję: ");
+            int wybor = input.nextInt();
+
+        }
 }
 
