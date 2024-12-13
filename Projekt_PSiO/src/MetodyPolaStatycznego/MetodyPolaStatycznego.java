53c8eb82356e6f3bc6a5887e536d93e746d9971a
StudentBJ <stawiamklocki@onet.pl>
2024-12-13 15:52:01 +0100

    Main Nowa zmiana

---
+++ Projekt_PSiO/src/MetodyPolaStatycznego/MetodyPolaStatycznego.java
@@ -0,0 +1,14 @@
+public class MetodyPolaStatycznego{
+
+    public static void wczytajBazeDanych()
+    {
+        File baza_klienci = new File("BazaDanych\\klienci.txt");
+        File baza_sprzedawcy = new File("BazaDanych\\sprzedawcy.txt");
+        File baza_pracownicy = new File("BazaDanych\\pracownicy.txt");
+        File baza_produkty = new File("BazaDanych\\produkty.txt");
+
+    }
+
+
+
+}
