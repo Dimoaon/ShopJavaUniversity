32a549ee31de57b46c8aa078d84a6e77a38d0636
StudentBJ <stawiamklocki@onet.pl>
2024-12-18 14:20:28 +0100

    Create Produkty.java

---
+++ Projekt_PSiO/src/Produktyyyyy/Produkty.java
@@ -0,0 +1,51 @@
+package Produkty;
+
+public abstract class Produkty{
+	
+	String nazwaProduktu;
+	float cenaProduktu;
+	int liczbaProduktu;
+	String opisProduktu;
+	
+	public Produkty(String nazwaProduktu, float cenaProduktu, int liczbaProduktu, String opisProduktu) {
+		this.nazwaProduktu = nazwaProduktu;
+		this.cenaProduktu = cenaProduktu;
+		this.liczbaProduktu = liczbaProduktu;
+		this.opisProduktu = opisProduktu;
+	}
+	
+	public String getNazwaProduktu() {
+		return nazwaProduktu;
+	}
+	public void setNazwaProduktu(String nazwaProduktu) {
+		this.nazwaProduktu = nazwaProduktu;
+	}
+	public float getCenaProduktu() {
+		return cenaProduktu;
+	}
+	public void setCenaProduktu(float cenaProduktu) {
+		this.cenaProduktu = cenaProduktu;
+	}
+	public int getLiczbaProduktu() {
+		return liczbaProduktu;
+	}
+	public void setLiczbaProduktu(int liczbaProduktu) {
+		this.liczbaProduktu = liczbaProduktu;
+	}
+	public String getOpisProduktu() {
+		return opisProduktu;
+	}
+	public void setOpisProduktu(String opisProduktu) {
+		this.opisProduktu = opisProduktu;
+	}
+
+
+	public String toString() {
+		return "Produkty [nazwaProduktu=" + nazwaProduktu + ", cenaProduktu=" + cenaProduktu + ", liczbaProduktu="
+				+ liczbaProduktu + ", opisProduktu=" + opisProduktu;
+	}
+	
+	
+
+	
+}
