9604544b8f22c90c3e61082dccfa5e39076d6550
StudentBJ <stawiamklocki@onet.pl>
2025-01-08 17:47:54 +0100

    Ikona dla pracownikaGUI

--- Projekt_PSiO/src/strategiaGUI/PracownikGUI.java
+++ Projekt_PSiO/src/strategiaGUI/PracownikGUI.java
@@ -1,5 +1,7 @@
 package strategiaGUI;
 
+import javax.imageio.ImageIO;
+import java.io.File;
 import javax.swing.JFrame;
 
 public class PracownikGUI extends OsobaZarzadzajacaGUI {
@@ -11,6 +13,11 @@ public class PracownikGUI extends OsobaZarzadzajacaGUI {
 	@Override
 	public void GUIcreate(JFrame frame1) {
 		super.GUIcreate(frame1);
+		try {
+		    frame1.setIconImage(ImageIO.read(new File("Grafika/dolarNiebieski.png")));
+		} catch (Exception e) {
+		    System.err.println("Błąd podczas wczytywania ikony: " + e.getMessage());
+		}
 		
 		// TODO wykorzystać wspólne menu dla osoby zarządzającej + dodać funkcje pracownika
 
