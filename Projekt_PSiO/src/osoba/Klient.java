ee8356a4e47720e45b1cc7ce693d2ddd89587689
moshenetsb <bogdan.moshenets@outlook.com>
2025-01-09 22:50:47 +0100

    Poprawiono błędy w metodzie kup (Klient)

--- Projekt_PSiO/src/osoba/Klient.java
+++ Projekt_PSiO/src/osoba/Klient.java
@@ -59,33 +59,53 @@ public class Klient extends Osoba {
 	}
 
 	public void kup(JFrame frame1) {
-		koszyk.setCenaDoZaplaty(koszyk.obliczCene() * (1 + this.promocjaKlienta.promocja() / 100.0));
-		if (super.getSaldoKonta() < koszyk.getCenaDoZaplaty()) {
-			JOptionPane.showMessageDialog(frame1, "Brakuje pięniędzy! Doładuj konto i wróć.", "Proces kupowania",
-					JOptionPane.ERROR_MESSAGE);
-		} else if (czyWystarczyTowarow(frame1)) {
-			this.historiaZakupow.add(koszyk);
-			this.koszyk = new Zakupy();
-			JOptionPane.showMessageDialog(frame1, "Dziękujemy za zakupy!", "Proces kupowania",
-					JOptionPane.INFORMATION_MESSAGE);
+
+		if (czyWystarczyTowarow(frame1)) {
+
+			koszyk.setCenaDoZaplaty(koszyk.obliczCene() * (1 - this.promocjaKlienta.promocja() / 100.0));
+
+			if (super.getSaldoKonta() < koszyk.getCenaDoZaplaty()) {
+				JOptionPane.showMessageDialog(frame1, "Brakuje pięniędzy! Doładuj konto i wróć.", "Proces kupowania",
+						JOptionPane.ERROR_MESSAGE);
+
+			} else {
+				this.historiaZakupow.add(koszyk);
+				this.koszyk = new Zakupy();
+				JOptionPane.showMessageDialog(frame1, "Dziękujemy za zakupy!", "Proces kupowania",
+						JOptionPane.INFORMATION_MESSAGE);
+			}
+
 		}
 	}
 
 	private boolean czyWystarczyTowarow(JFrame frame1) {
+
 		for (Produkty produkt : koszyk.getListaProduktow()) {
+
 			Produkty temp = Metody.znajdzProduktPoID(produkt.getIdProduktu());
 
 			if (temp == null) {
+
 				JOptionPane.showMessageDialog(frame1,
 						"Produkt " + produkt.getNazwaProduktu()
-								+ "już nie sprzedaje się z sklepie. On został automatycznie usunięty z koszyka.",
+								+ " już nie sprzedaje się w sklepie. On został automatycznie usunięty z koszyka.",
 						"Proces kupowania", JOptionPane.INFORMATION_MESSAGE);
 				koszyk.getListaProduktow().remove(koszyk.getListaProduktow().indexOf(produkt));
+
 			} else if (produkt.getLiczbaProduktu() <= temp.getLiczbaProduktu()) {
 				JOptionPane.showMessageDialog(frame1,
 						"Brakuje produktu " + produkt.getNazwaProduktu()
 								+ ". Usuń ten produkt z koszyka lub zmień ilość i powtórz kupowanie",
 						"Proces kupowania", JOptionPane.ERROR_MESSAGE);
+
+				// Pytanie o usunięcie produktu
+				int czyUsunacTenProdukt = JOptionPane.showConfirmDialog(frame1,
+						"Czy chcesz usunąć " + produkt.getNazwaProduktu() + "?", "Proces kupowania",
+						JOptionPane.YES_NO_OPTION);
+
+				if (czyUsunacTenProdukt == JOptionPane.YES_OPTION)
+					koszyk.getListaProduktow().remove(koszyk.getListaProduktow().indexOf(produkt));
+
 				return false;
 			}
 		}
