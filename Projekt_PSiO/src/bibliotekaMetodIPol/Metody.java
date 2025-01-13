b688f80b700582dd87e49bc74ebb657e173ac390
StudentBJ <stawiamklocki@onet.pl>
2025-01-13 10:55:41 +0100

    Update Metody.java
    isValid w Metodach

--- Projekt_PSiO/src/bibliotekaMetodIPol/Metody.java
+++ Projekt_PSiO/src/bibliotekaMetodIPol/Metody.java
@@ -96,4 +96,85 @@ public class Metody {
 		return false;
 	}
 
+	public static boolean isValidData(JFrame frame1, String email, String haslo, String login, String nazwisko, String imie, String wiekStr) {
+		if (email.isEmpty() || haslo.isEmpty() || login.isEmpty() || nazwisko.isEmpty() || imie.isEmpty()
+				|| wiekStr.isEmpty()) {
+			JOptionPane.showMessageDialog(frame1, "Wszystkie pola muszą być wypełnione!", "Błąd",
+					JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+
+		if (!email.contains("@")) {
+			JOptionPane.showMessageDialog(frame1, "Brak @ w emailu", "Błąd", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+
+		boolean emailExists = true;
+		boolean loginExists = true;
+
+		if (MenuLogowanie.szukajIDEmailKlienta(email) == -1 && MenuLogowanie.szukajIDEmailZarzadzajacych(email) == -1)
+			emailExists = false;
+
+		if (MenuLogowanie.szukajIDLoginKlienta(login) == -1 && MenuLogowanie.szukajIDLoginZarzadzajacych(login) == -1)
+			loginExists = false;
+
+		if (emailExists && loginExists) {
+			JOptionPane.showMessageDialog(frame1, "Konto z podanym emailem i loginem już istnieje!", "Błąd",
+					JOptionPane.ERROR_MESSAGE);
+			return false;
+		} else if (emailExists) {
+			JOptionPane.showMessageDialog(frame1, "Konto z podanym emailem już istnieje!", "Błąd",
+					JOptionPane.ERROR_MESSAGE);
+			return false;
+		} else if (loginExists) {
+			JOptionPane.showMessageDialog(frame1, "Konto z podanym loginem już istnieje!", "Błąd",
+					JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+
+		String[] znakiSpecjalne = { "!", "@", "#", "$", "%", "^", "&", "*", "(", ")" };
+		int liczbaSpecjalnych = 0;
+		int liczbaZnakow = haslo.length();
+		int liczbaMalychZnakow = 0;
+		int liczbaDuzychZnakow = 0;
+
+		for (String znak : znakiSpecjalne) {
+			if (haslo.contains(znak)) {
+				liczbaSpecjalnych++;
+			}
+		}
+
+		for (int i = 0; i < haslo.length(); i++) {
+			char znak = haslo.charAt(i);
+			if (Character.isLowerCase(znak)) {
+				liczbaMalychZnakow++;
+			} else if (Character.isUpperCase(znak)) {
+				liczbaDuzychZnakow++;
+			}
+		}
+
+		if (liczbaZnakow < 8 || liczbaMalychZnakow < 3 || liczbaDuzychZnakow < 3 || liczbaSpecjalnych < 3) {
+			JOptionPane.showMessageDialog(frame1,
+					"Hasło nie spełnia standardów bezpieczeństwa.\nPowinno mieć minimum: 8 znaków (w tym po 3 znaki małe i duże), 3 różne znaki specjalne",
+					"Błąd", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+
+		if (!isNumeric(wiekStr)) {
+			JOptionPane.showMessageDialog(frame1, "Wiek musi być liczbą!", "Błąd", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+
+		return true;
+	}
+
+	public static boolean isNumeric(String str) {
+		try {
+			Integer.parseInt(str);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
 }
