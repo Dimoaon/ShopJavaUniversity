41911e76f03287ac7c1444f4ab342edd7817f884
StudentBJ <stawiamklocki@onet.pl>
2025-01-13 10:54:06 +0100

    Update Rejestracja.java
    (isValid do Metody))

--- Projekt_PSiO/src/strategiaGUI/Rejestracja.java
+++ Projekt_PSiO/src/strategiaGUI/Rejestracja.java
@@ -13,6 +13,7 @@ import zakupy.Zakupy;
 import java.awt.*;
 import java.io.File;
 import java.util.ArrayList;
+import bibliotekaMetodIPol.*;
 
 public class Rejestracja implements GUIstrategia {
 
@@ -95,7 +96,7 @@ public class Rejestracja implements GUIstrategia {
 		String imie = imieField.getText().trim();
 		String wiek = wiekField.getText().trim();
 
-		if (isValidData(email, haslo, login, nazwisko, imie, wiek)) {
+		if (Metody.isValidData(frame1, email, haslo, login, nazwisko, imie, wiek)) {
 			Klient newClient = new Klient(email, haslo, login, nazwisko, imie, Integer.parseInt(wiek),
 					new Adres("", "", "", "", "", ""), 0, new PromocjaPodstawowa(), new ArrayList<>(), new Zakupy());
 			Metody.getListaKlientow().add(newClient);
@@ -110,87 +111,7 @@ public class Rejestracja implements GUIstrategia {
 
 	}
 
-	private boolean isValidData(String email, String haslo, String login, String nazwisko, String imie,
-			String wiekStr) {
-		if (email.isEmpty() || haslo.isEmpty() || login.isEmpty() || nazwisko.isEmpty() || imie.isEmpty()
-				|| wiekStr.isEmpty()) {
-			JOptionPane.showMessageDialog(frame1, "Wszystkie pola muszą być wypełnione!", "Błąd",
-					JOptionPane.ERROR_MESSAGE);
-			return false;
-		}
-
-		if (!email.contains("@")) {
-			JOptionPane.showMessageDialog(frame1, "Brak @ w emailu", "Błąd", JOptionPane.ERROR_MESSAGE);
-			return false;
-		}
-
-		boolean emailExists = true;
-		boolean loginExists = true;
-
-		if (MenuLogowanie.szukajIDEmailKlienta(email) == -1 && MenuLogowanie.szukajIDEmailZarzadzajacych(email) == -1)
-			emailExists = false;
-
-		if (MenuLogowanie.szukajIDLoginKlienta(login) == -1 && MenuLogowanie.szukajIDLoginZarzadzajacych(login) == -1)
-			loginExists = false;
-
-		if (emailExists && loginExists) {
-			JOptionPane.showMessageDialog(frame1, "Konto z podanym emailem i loginem już istnieje!", "Błąd",
-					JOptionPane.ERROR_MESSAGE);
-			return false;
-		} else if (emailExists) {
-			JOptionPane.showMessageDialog(frame1, "Konto z podanym emailem już istnieje!", "Błąd",
-					JOptionPane.ERROR_MESSAGE);
-			return false;
-		} else if (loginExists) {
-			JOptionPane.showMessageDialog(frame1, "Konto z podanym loginem już istnieje!", "Błąd",
-					JOptionPane.ERROR_MESSAGE);
-			return false;
-		}
-
-		String[] znakiSpecjalne = { "!", "@", "#", "$", "%", "^", "&", "*", "(", ")" };
-		int liczbaSpecjalnych = 0;
-		int liczbaZnakow = haslo.length();
-		int liczbaMalychZnakow = 0;
-		int liczbaDuzychZnakow = 0;
-
-		for (String znak : znakiSpecjalne) {
-			if (haslo.contains(znak)) {
-				liczbaSpecjalnych++;
-			}
-		}
-
-		for (int i = 0; i < haslo.length(); i++) {
-			char znak = haslo.charAt(i);
-			if (Character.isLowerCase(znak)) {
-				liczbaMalychZnakow++;
-			} else if (Character.isUpperCase(znak)) {
-				liczbaDuzychZnakow++;
-			}
-		}
-
-		if (liczbaZnakow < 8 || liczbaMalychZnakow < 3 || liczbaDuzychZnakow < 3 || liczbaSpecjalnych < 3) {
-			JOptionPane.showMessageDialog(frame1,
-					"Hasło nie spełnia standardów bezpieczeństwa.\nPowinno mieć minimum: 8 znaków (w tym po 3 znaki małe i duże), 3 różne znaki specjalne",
-					"Błąd", JOptionPane.ERROR_MESSAGE);
-			return false;
-		}
-
-		if (!isNumeric(wiekStr)) {
-			JOptionPane.showMessageDialog(frame1, "Wiek musi być liczbą!", "Błąd", JOptionPane.ERROR_MESSAGE);
-			return false;
-		}
-
-		return true;
-	}
-
-	private boolean isNumeric(String str) {
-		try {
-			Integer.parseInt(str);
-			return true;
-		} catch (NumberFormatException e) {
-			return false;
-		}
-	}
+	
 
 	private void returnToLogin() {
 		Metody.setWybraneGUI(new LoginGUI(frame1));
